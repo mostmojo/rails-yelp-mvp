5e958c98376afb3f6ab643012004b983dcea5008
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-02-20 14:16:42 +0000

    Add restaurant_id to reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_122026) do
+ActiveRecord::Schema.define(version: 2019_02_20_140959) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2019_02_20_122026) do
     t.text "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "restaurant_id"
   end
 
 end
